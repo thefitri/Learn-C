0e04459bf4be2b01f8017bf51ce5f26d940c4f94
thefitriali <6263441+thefitriali@users.noreply.github.com>
2018-05-24 11:33:52 +0800

    Completed Exercise 11

--- code/ex8.c
+++ code/ex8.c
@@ -5,12 +5,14 @@ int main (int argc, char *argv[])
 	int i = 0 ;
 
 	if (argc == 1) {
+		printf("You have no argument.\n");
+	} else if (argc == 2) {
 		printf("You only have one argument. You Suck.\n");
-	} else if (argc > 1 && argc < 4) {
+	} else if (argc > 2 && argc < 5) {
 		printf("Here's your agruments:\n");
 
-		for (i = 0; i < argc; i++) {
-			printf("%s ", argv[i]);
+		for (i = 1; i < argc; i++) {
+			printf("%s\n", argv[i]);
 		}
 		printf("\n");
 	} else {
