4cbe9165a953eb46a133dded49c6fa6db16a4b7e
thefitriali <6263441+thefitriali@users.noreply.github.com>
2018-05-24 15:21:37 +0800

    Completed extra credit for exercise 14

---
+++ code/ex14.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// forward declarations
+void print_letters(char arg[], int length);
+
+void print_arguments(int argc, char *argv[])
+{
+	int i =0;
+
+	for (i = 0; i < argc; i++) {
+		int word_length = strlen(argv[i]);
+		print_letters(argv[i], word_length);
+	}
+}
+
+void print_letters(char arg[], int word_length)
+{
+	int i = 0;
+	
+	for (i = 0; i < word_length; i++) {
+		char ch = arg[i];
+
+		if (isalpha(ch) || isblank(ch)) {
+			printf("'%c' == %d\n", ch, ch);
+		}
+	}
+
+	printf("\n");
+}
+
+int main (int argc, char *argv[])
+{
+	print_arguments(argc, argv);
+	return 0;
+}
